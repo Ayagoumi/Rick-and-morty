aa463fb8bfe7a7d3713cc6cd471a89f8dfbc1b2c
Ayagoumi <agoumi94@gmail.com>
2022-10-08 13:22:29 +0100

    Remove dark mode support

--- src/@types/settings.ts
+++
@@ -1,6 +0,0 @@
-export type ThemeMode = "light" | "dark";
-
-export type SettingsContextProps = {
-  themeMode: ThemeMode;
-  onChangeMode: (event: React.ChangeEvent<HTMLInputElement>) => void;
-};
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,33 +1,24 @@
-import { useMemo, ReactNode } from 'react';
-import { CssBaseline } from '@mui/material';
-import { createTheme, ThemeOptions, ThemeProvider } from '@mui/material/styles';
-import useSettings from '../hooks/useSettings';
-import palette from './palette';
-import shadows, { customShadows } from './shadows';
-import componentsOverride from './overrides/index';
-import shape from './shape';
-import typography from './typography';
+import { ReactNode } from "react";
+import { CssBaseline } from "@mui/material";
+import { createTheme, ThemeOptions, ThemeProvider } from "@mui/material/styles";
+import palette from "./palette";
+import shadows, { customShadows } from "./shadows";
+import componentsOverride from "./overrides/index";
+import shape from "./shape";
+import typography from "./typography";
 
 type ThemeConfigProps = {
   children: ReactNode;
 };
 
 export default function ThemeConfig({ children }: ThemeConfigProps) {
-  const { themeMode } = useSettings();
-  const isLight = themeMode === 'light';
-
-  const themeOptions: ThemeOptions = useMemo(
-    () => ({
-      shape: shape,
-      palette: isLight
-        ? { ...palette.light, mode: 'light' }
-        : { ...palette.dark, mode: 'dark' },
-      shadows: shadows.light,
-      customShadows: customShadows.light,
-      typography: typography,
-    }),
-    [isLight]
-  );
+  const themeOptions: ThemeOptions = {
+    shape: shape,
+    palette: { ...palette.light, mode: "light" },
+    shadows: shadows.light,
+    customShadows: customShadows.light,
+    typography: typography,
+  };
 
   const theme = createTheme(themeOptions);
   // @ts-ignore
